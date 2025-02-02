798161fbdaf48a2777d37ce45f5e73bc3ba0b6ad
Roy Levi <roy3@post.bgu.ac.il>
2025-02-03 01:21:06 +0200

    chore: comment out unused code in bgu4u-content.js for clarity

--- scripts/bgu4u-content.js
+++ scripts/bgu4u-content.js
@@ -1,22 +1,22 @@
-console.log('bgu4u content script loaded');
-//print the tab id
-if(document.readyState === 'loading') {
-    document.addEventListener('DOMContentLoaded', validateCourse);
-} else {
-    validateCourse();
-}
+// console.log('bgu4u content script loaded');
+// //print the tab id
+// if(document.readyState === 'loading') {
+//     document.addEventListener('DOMContentLoaded', validateCourse);
+// } else {
+//     validateCourse();
+// }
 
-function validateCourse() {
-    const frame = document.querySelector('frame[name="main"]');
+// function validateCourse() {
+//     const frame = document.querySelector('frame[name="main"]');
 
-    frame.onload = function () {
-        setTimeout(() => {
-            const win = frame.contentWindow;
-            if (win && typeof win.goType === "function") {
-                console.log(win.goType(1));
-            } else {
-                console.error("goType is still not available.");
-            }
-        }, 1000); // Wait 1 second
-    };
-}
+//     frame.onload = function () {
+//         setTimeout(() => {
+//             const win = frame.contentWindow;
+//             if (win && typeof win.goType === "function") {
+//                 console.log(win.goType(1));
+//             } else {
+//                 console.error("goType is still not available.");
+//             }
+//         }, 1000); // Wait 1 second
+//     };
+// }
