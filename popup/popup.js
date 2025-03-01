b862b7f633ecfcbcab8b67935c0689ea66096436
Roy Levi <roy3@post.bgu.ac.il>
2025-03-01 23:44:38 +0200

    fix: correct SVG path for loading icon in popup

--- popup/popup.js
+++ popup/popup.js
@@ -183,7 +183,7 @@ document.addEventListener("DOMContentLoaded", function () {
             openGraphBtn.disabled = true;
 
             const loadingPaths = [
-                '<path d="M1 11a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v3a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1zm5-4a1"/>',
+                '<path d="M1 11a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v3a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1z"/>',
                 '<path d="M6 7a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v7a1 1 0 0 1-1 1H7a1 1 0 0 1-1-1z"/>',
                 '<path d="M11 2a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1h-2a1 1 0 0 1-1-1z"/>'
             ];
